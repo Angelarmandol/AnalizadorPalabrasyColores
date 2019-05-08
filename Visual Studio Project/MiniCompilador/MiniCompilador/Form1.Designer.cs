c595f301d1a184593d6d3e8ef17dc239ce0bbb1c
Angel Lopez <angelarmandol@hotmail.com>
2019-05-08 00:57:01 -0700

    add compile

--- Visual Studio Project/MiniCompilador/MiniCompilador/Form1.Designer.cs
+++ Visual Studio Project/MiniCompilador/MiniCompilador/Form1.Designer.cs
@@ -42,8 +42,11 @@
             this.Tabs = new System.Windows.Forms.TabControl();
             this.TABDebug = new System.Windows.Forms.TabPage();
             this.TABConsole = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.menuStrip1.SuspendLayout();
             this.Tabs.SuspendLayout();
+            this.TABDebug.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -72,25 +75,25 @@
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.saveToolStripMenuItem.Text = "Save";
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.openToolStripMenuItem.Text = "Open";
             // 
             // printToolStripMenuItem
             // 
             this.printToolStripMenuItem.Name = "printToolStripMenuItem";
-            this.printToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.printToolStripMenuItem.Text = "Print";
             // 
             // newToolStripMenuItem
             // 
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.newToolStripMenuItem.Text = "New";
             this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
             // 
@@ -108,11 +111,12 @@
             this.compileToolStripMenuItem.Name = "compileToolStripMenuItem";
             this.compileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.compileToolStripMenuItem.Text = "Compile";
+            this.compileToolStripMenuItem.Click += new System.EventHandler(this.compileToolStripMenuItem_Click);
             // 
             // runToolStripMenuItem
             // 
             this.runToolStripMenuItem.Name = "runToolStripMenuItem";
-            this.runToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
             this.runToolStripMenuItem.Text = "Run";
             // 
             // helpToolStripMenuItem
@@ -129,6 +133,7 @@
             this.TBCode.Name = "TBCode";
             this.TBCode.Size = new System.Drawing.Size(282, 262);
             this.TBCode.TabIndex = 1;
+            this.TBCode.TextChanged += new System.EventHandler(this.TBCode_TextChanged);
             // 
             // Tabs
             // 
@@ -144,6 +149,7 @@
             // 
             // TABDebug
             // 
+            this.TABDebug.Controls.Add(this.richTextBox1);
             this.TABDebug.Location = new System.Drawing.Point(4, 22);
             this.TABDebug.Name = "TABDebug";
             this.TABDebug.Padding = new System.Windows.Forms.Padding(3);
@@ -151,6 +157,7 @@
             this.TABDebug.TabIndex = 0;
             this.TABDebug.Text = "Debug";
             this.TABDebug.UseVisualStyleBackColor = true;
+            this.TABDebug.Click += new System.EventHandler(this.TABDebug_Click);
             // 
             // TABConsole
             // 
@@ -162,20 +169,43 @@
             this.TABConsole.Text = "Console";
             this.TABConsole.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Firebrick;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(154, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 24);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 6);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(287, 50);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(306, 385);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.Tabs);
             this.Controls.Add(this.TBCode);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Mini Compilador";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.Tabs.ResumeLayout(false);
+            this.TABDebug.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -197,6 +227,8 @@
         private System.Windows.Forms.TabControl Tabs;
         private System.Windows.Forms.TabPage TABDebug;
         private System.Windows.Forms.TabPage TABConsole;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- Visual Studio Project/MiniCompilador/MiniCompilador/Form1.cs
+++ Visual Studio Project/MiniCompilador/MiniCompilador/Form1.cs
@@ -10,21 +10,23 @@ using System.Windows.Forms;
 
 namespace MiniCompilador
 {
+
+    
     public partial class Form1 : Form
     {
         public Form1()
-        {
+        { 
             InitializeComponent();
         }
-
+        CompilerClass compiler = new CompilerClass();
         private void newToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (TBCode.Text == "")
             {
                 TBCode.Text = "";
             }else{
-                string message = "¿Deseas salvarlo en un archivo nuevo antes de crear uno nuevo?";
-                string title = "Salvar Archivo";
+                string message = "Do you want to save in another File before create another one?";
+                string title = "Save File";
                 MessageBoxButtons buttons = MessageBoxButtons.YesNo;
                 DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
                 if (result == DialogResult.No){
@@ -43,5 +45,50 @@ namespace MiniCompilador
             Help1 help2 = new Help1();
             help2.Show();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this is the code area
+
+        }
+
+        private void compileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Se enviara a: "+TBCode.Text);
+          WritteOnConsole("Compiled with: "+compiler.sanitize(TBCode.Text)+" errors");
+        }
+
+        public void TBCode_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TBCode.Text = "Inicio;\r\n";
+            TBCode.Text += "Mensaje[hola];\r\n";
+            TBCode.Text += "Contador[6];\r\n";
+            TBCode.Text += "Fin;";
+            
+        }
+
+        private void TABDebug_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WritteOnConsole(string text)
+        {
+            richTextBox1.Text = text;
+        }
     }
+
+   
 }
+
+
+
+
+
+
+
