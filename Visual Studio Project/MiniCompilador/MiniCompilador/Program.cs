5e200d7589e948c550c33c025bf54570fa7681a6
Angel Lopez <angelarmandol@hotmail.com>
2019-05-17 01:00:48 -0700

    Revert "add run and test button"
    This reverts commit ba273d6bb46e837e999dd779218c477665a3e200.

--- Visual Studio Project/MiniCompilador/MiniCompilador/Program.cs
+++ Visual Studio Project/MiniCompilador/MiniCompilador/Program.cs
@@ -12,13 +12,11 @@ namespace MiniCompilador
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
-        
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-           
         }
     }
 }
